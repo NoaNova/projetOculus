eea55500128b4b0d410019494da72c79a920f186
Thorbjornen <niels.groeninck4@gmail.com>
2026-01-08 10:17:48 +0100

    zone destruction

--- Assets/WasteSortingCenterPack/Scripts/DestructeurDechets.cs
+++ Assets/WasteSortingCenterPack/Scripts/DestructeurDechets.cs
@@ -2,19 +2,14 @@ using UnityEngine;
 
 public class DestructeurDechets : MonoBehaviour
 {
-    // Cette fonction magique est appelée automatiquement par Unity
-    // quand UNIQUEMENT un objet entre dans une zone "Is Trigger".
     private void OnTriggerEnter(Collider other)
     {
-        // 1. Vérification de sécurité :
-        // Est-ce que l'objet qui vient d'entrer a bien le badge "Dechet" ?
-        if (other.CompareTag("Trash"))
+        // On vérifie si l'objet a l'étiquette "Plastique" OU (||) l'étiquette "Normal"
+        // On garde aussi "Trash" au cas où tu as oublié de changer certains objets.
+        if (other.CompareTag("Plastique") || other.CompareTag("Carton") || other.CompareTag("Trash"))
         {
-            // 2. Si oui, on détruit l'objet entier (gameObject).
+            // On détruit l'objet pour nettoyer le jeu
             Destroy(other.gameObject);
-
-            // (Optionnel) Tu pourrais ajouter du son ou des points ici plus tard !
-            // Debug.Log("Déchet détruit !"); 
         }
     }
 }
